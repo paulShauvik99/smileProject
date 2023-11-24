3cdc872afabcbeec05ad34189a027d6d998f1ae6
gdas <gdas37467@gmail.com>
2023-11-25 03:54:50 +0530

    rendifications

--- Blood-bank-backend/donor/urls.py
+++ Blood-bank-backend/donor/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('send_otp/',views.send_otp),
     path('verify_otp/',views.verify_otp),
     path('confirm_donor/',views.confirm_donor),
+    path('get_donor_records/',views.get_donor_records),
     path('get_matched_donors/',views.get_matched_donors),
     path('get_confirmed_donors/',views.get_confirmed_donors)
 ]
