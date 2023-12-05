e8754e77cc65d85bb39f7b44d6ba8a11e82c1310
Shauvik Paul <paulsayar99@gmail.com>
2023-12-06 00:56:07 +0530

    FrontEnd Updated 2

--- Blood-bank-backend/donor/views.py
+++ Blood-bank-backend/donor/views.py
@@ -192,7 +192,7 @@ def verify_otp(request):
             
             type = jwt.encode({'isDonor': isDonor,"isRecipient" : isRecipient}, key, algorithm='HS256')
 
-            request.session.set_expiry(3000000)
+            request.session.set_expiry(20*60)
             
             if status == False:
                 return JsonResponse({"error" : "Incorrect OTP"  },status=400)
@@ -572,7 +572,8 @@ def admin_login(request):
             if user is not None:
                 # Log in the authenticated user
                 login(request, user)
-              
+                request.session.set_expiry(45*60)
+
                 is_staff = user.is_superuser
                 #newline update
 
