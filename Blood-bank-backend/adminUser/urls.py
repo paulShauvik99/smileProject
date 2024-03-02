114ebc35698364292494ed71e23ce42aa886a212
gdas <gdas37467@gmail.com>
2024-03-03 03:01:07 +0530

    Final API added

--- Blood-bank-backend/adminUser/urls.py
+++ Blood-bank-backend/adminUser/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     path('admin_logout/',views.admin_logout),
     path('send_requirement/<str:donor_id>',views.requirement_msg),
     path('loan_msg/<str:donor_id>',views.loan_msg),
-    path('confirm_loan/<str:donor_id>',views.confirm_loan)
+    path('confirm_loan/<str:donor_id>',views.confirm_loan),
+    path('addPhotos/',views.addPhotos),
+    path('getLeaderboardImages/',views.getLeaderboardImage)
 ]
